1c5fbc4d018d67c89219890fa5345dc366b96365
Nilkanth <me@nilkanthpatel.com>
2013-11-25 21:59:34 -0500

    Updated javascript to work with Parse

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+Parse.initialize("YSYKURfsGILaLBqVUnd1ViJqEL0FG05ng8qTI6OQ", "nGmU13RtnJqGIhrr5RQeHA6Th9wf23rc7tJwgw1J");
+var Itineraries = Parse.Object.extend("Itineraries");
+
+var query = new Parse.Query(Itineraries);
+	query.find({
+    	success:function(results) {
+    		window.allItineraries = results;
+    		window.currentItin = window.allItineraries[0].id;
+    	}
+	});
+
 window.onload = function() {
 	$("#venueName").change(function() {
 		var query = $("#venueName").val();
@@ -24,4 +35,37 @@ function displayVenues(theList) {
 		finalHTML = finalHTML+'<li><span class="title">'+name+' </span><span class="location">AT '+address+'</span></li>';
 	}
 	$("#venueList").html(finalHTML);
+	addVenue(theList.response.venues[0],window.currentItin);
+}
+
+function addItin() {
+	
+}
+
+function addVenue(venue,itinToAddTo) {
+	query.get(itinToAddTo, {
+		success: function(itinObject) {
+			var currentVenues = itinObject.get("venues");
+	    	itinObject.save(null, {
+				success: function(itinObject) {
+					if (currentVenues == undefined) {
+						itinObject.set("venues", [venue]);
+						itinObject.save();
+					} else {
+						currentVenues.push(venue);
+						itinObject.set("venues", currentVenues);
+						itinObject.save();	
+					}
+				}
+			});
+		}
+  	});
+}
+
+function displayItin(itinID) {
+	query.get(itinID, {
+		success: function(gameScore) {
+	    
+		}
+  	});
 }

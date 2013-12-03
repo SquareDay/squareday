6c00bf4b89789f1c1e9de5c7a1d204710864a5d5
William Falk-Wallace <wfalkwallace@gmail.com>
2013-12-03 17:10:06 -0500

    fixes accidental delete of sorter

--- js/itinerary.js
+++ js/itinerary.js
@@ -8,6 +8,7 @@ function displayItin(itinToDisplay) {
 			console.log(venues);
 			var markerObject = new Array();
 			if (venues != undefined) {
+				venues.sort(function(a,b){return a.timeStart - b.timeStart});
 				for (var i=0; i<venues.length; i++) {
 					finalHTML = finalHTML+'<div class="panel panel-primary"><div class="panel-heading"><div class="row"><div class="col-md-6"><h3 class="panel-title">'+venues[i].name+'</h3></div><div class="col-md-6 text-right"><span class="event-btn glyphicon glyphicon-plus-sign"></span><span class="event-btn glyphicon glyphicon-minus-sign"></span><span class="event-btn glyphicon glyphicon-question-sign"></span></div></div></div><div class="panel-body"><span class="event-time pull-left">'+venues[i].timeStart+' - '+venues[i].timeEnd+'</span><br><p>'+venues[i].location.address+'</p><p>'+venues[i].description+'</p></div></div>';					
 					var currentMarkerObject = { type: 'Feature', geometry: { type: 'Point', coordinates: [venues[i].location.lng, venues[i].location.lat]}, properties: { title: venues[i].name } };

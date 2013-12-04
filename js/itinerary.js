cb5931eb44d6c69ee9bed497d4aba843d0636ac0
William Falk-Wallace <wfalkwallace@gmail.com>
2013-12-04 01:44:44 -0500

    sort stuffs

--- js/itinerary.js
+++ js/itinerary.js
@@ -5,11 +5,12 @@ function displayItin(itinToDisplay) {
 			$("#introSplash").hide();
 			var finalHTML = "<h2>My "+itinObject.get("name")+" Itinerary</h2>";
 			var venues = itinObject.get("venues");
-			console.log(venues);
 			var markerObject = new Array();
 			if (venues != undefined) {
-				venues.sort(function(a,b){return a.timeStart - b.timeStart});
+				venues.sort(function(a,b){console.dir(a.timeStart - b.timeStart); return a.timeStart - b.timeStart});
+				console.log(venues);
 				for (var i=0; i<venues.length; i++) {
+					console.log(venues[i].timeStart); 
 					finalHTML = finalHTML+'<div class="panel panel-primary"><div class="panel-heading"><div class="row"><div class="col-md-6"><h3 class="panel-title">'+venues[i].name+'</h3></div><div class="col-md-6 text-right"><span class="event-btn glyphicon glyphicon-plus-sign"></span><span class="event-btn glyphicon glyphicon-minus-sign"></span><span class="event-btn glyphicon glyphicon-question-sign"></span></div></div></div><div class="panel-body"><span class="event-time pull-left">'+venues[i].timeStart+' - '+venues[i].timeEnd+'</span><br><p>'+venues[i].location.address+'</p><p>'+venues[i].description+'</p></div></div>';					
 					var currentMarkerObject = { type: 'Feature', geometry: { type: 'Point', coordinates: [venues[i].location.lng, venues[i].location.lat]}, properties: { title: venues[i].name } };
 					markerObject.push(currentMarkerObject);
